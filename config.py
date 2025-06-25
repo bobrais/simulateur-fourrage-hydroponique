092093d2e12ed6b7c2956a5e215f14f745cb47e8
bobrais <bobrace16@gmail.com>
2025-06-25 11:32:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+# config.py
 
+GRAINES = {
+    "orge":   {"rendement_cycle_kg": 7.5},
+    "blé":    {"rendement_cycle_kg": 6.0},
+    "maïs":   {"rendement_cycle_kg": 5.5},
+    "avoine": {"rendement_cycle_kg": 6.5},
+    "sorgho": {"rendement_cycle_kg": 5.8}
+}
