733f515a06008cd65af7e1fa72a5245f2b5c4509
bobrais <bobrace16@gmail.com>
2025-06-25 11:40:10 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
+# utils.py
 
+def demander_float(message):
+    try:
+        return float(input(message))
+    except:
+        return demander_float("🔁 Entrée invalide. Réessaye : ")
+
+def demander_choix(message, options):
+    val = input(message).lower()
+    return val if val in options else demander_choix(f"🔁 Choix invalide. Choisis parmi {options} : ", options)
